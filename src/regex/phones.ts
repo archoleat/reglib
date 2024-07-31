426abbff36b26debce1b0aa97dc8d87db7db4a1d
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-07-31 23:34:46 +0300

    feat: add new regex

--- src/regex/phones.ts
+++ src/regex/phones.ts
@@ -1,5 +1,6 @@
 const phones = {
   RU_REGEX: new RegExp(String.raw`^\+7 \((9\d{2,3})\) \d{3}-\d{2}-\d{2}$`),
+  US_REGEX: new RegExp(String.raw`^\+1 \((\d{3})\) \d{3}-\d{4}$`),
 };
 
 export { phones };
