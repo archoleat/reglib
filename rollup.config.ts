20bbf3ef08862d7f62d4382d66478f6bafe109f6
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-07-31 22:35:19 +0300

    fix: types

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,18 +1,47 @@
 import { defineConfig } from 'rollup';
 
+import { dts } from 'rollup-plugin-dts';
 import { minify } from 'rollup-plugin-esbuild';
 
+import alias from '@rollup/plugin-alias';
 import typescript from '@rollup/plugin-typescript';
 
+const sourceFolder = 'src';
 const fileName = 'index';
+const indexFile = `${fileName}.ts`;
 
 export default defineConfig([
   {
     plugins: [typescript(), minify()],
-    input: `src/${fileName}.ts`,
+    input: `${sourceFolder}/${indexFile}`,
     output: {
       file: `${fileName}.js`,
       format: 'es',
+    },
+  },
+  {
+    plugins: [
+      alias({
+        entries: [
+          {
+            find: '#font',
+            replacement: `${sourceFolder}/regex/font/${indexFile}`,
+          },
+          {
+            find: '#regex',
+            replacement: `${sourceFolder}/regex/${indexFile}`,
+          },
+          {
+            find: '#shared',
+            replacement: `${sourceFolder}/shared/${indexFile}`,
+          },
+        ],
+      }),
+      dts(),
+    ],
+    input: `${sourceFolder}/${indexFile}`,
+    output: {
+      file: `${fileName}.d.ts`,
     },
   },
 ]);
