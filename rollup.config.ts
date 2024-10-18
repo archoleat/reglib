4de615a35831b180a7e007dcc2e5f5e4754da5a0
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-10-18 22:06:29 +0300

    refactor: small changes

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,20 +1,15 @@
 import { resolve } from 'node:path';
-
 import { defineConfig } from 'rollup';
-
 import { dts } from 'rollup-plugin-dts';
 import { minify } from 'rollup-plugin-esbuild';
-
 import alias from '@rollup/plugin-alias';
 import typescript from '@rollup/plugin-typescript';
 
 const sourceFolder = 'src';
 const appFolder = `${sourceFolder}/app`;
 const sharedFolder = `${sourceFolder}/shared`;
-
 const fileFormat = 'es';
 const fileName = 'index';
-
 const declarationFile = `${fileName}.d.ts`;
 const entryFile = `${fileName}.ts`;
 const outputFile = `${fileName}.js`;
