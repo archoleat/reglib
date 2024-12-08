a15b9a5cac59bffa20cc085c77ba54f4d9312c5c
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2024-12-08 14:40:13 +0300

    refactor: structure

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
-export { FONT_FILE_NAME_REGEX, selectors } from '#validators';
+export { FONT_FILE_NAME_REGEX } from '#validators/font-file-name.ts';
+export { selectors } from '#validators/selectors.ts';
