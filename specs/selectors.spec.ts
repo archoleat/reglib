e1053b45116e6333bce4c4643e1b4040e2191483
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2024-12-15 19:19:39 +0300

    fix: tests

--- specs/selectors.spec.ts
+++ specs/selectors.spec.ts
@@ -6,8 +6,6 @@ describe('Nested Selectors Regex', async () => {
   const NESTED_ATTRIBUTE = selectors.nested.ATTRIBUTE_REGEX;
   const NESTED_CLASS = selectors.nested.CLASS_REGEX;
   const NESTED_MODIFIER = selectors.nested.MODIFIER_REGEX;
-  const CHILD_ATTRIBUTE = selectors.child.ATTRIBUTE_REGEX;
-  const CHILD_CLASS = selectors.child.CLASS_REGEX;
 
   spec('should &[attribute]', async () => {
     expect('&[attribute]').match(new RegExp(`&${NESTED_ATTRIBUTE}`));
@@ -79,21 +77,5 @@ describe('Nested Selectors Regex', async () => {
 
   spec('should &--modifier {}', async () => {
     expect('&--modifier {}').match(new RegExp(`&${NESTED_MODIFIER}`));
-  });
-
-  spec('should [attribute] &', async () => {
-    expect('[attribute] &').match(new RegExp(`&${CHILD_ATTRIBUTE}`));
-  });
-
-  spec('should [attribute] & {}', async () => {
-    expect('[attribute] & {}').match(new RegExp(`&${CHILD_ATTRIBUTE}`));
-  });
-
-  spec('should .class &', async () => {
-    expect('.class &').match(new RegExp(`&${CHILD_CLASS}`));
-  });
-
-  spec('should .class & {}', async () => {
-    expect('.class & {}').match(new RegExp(`&${CHILD_CLASS}`));
   });
 });
